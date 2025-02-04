092383c7be4e8d0a95bfd412d55dbdba4e89c7ec
Yongsik Im <lvhi7121@gmail.com>
2025-02-05 01:34:37 +0900

    디렉터리 구조 변경 및 CMake 추가

---
+++ Source/Engine/Core/Application.h
@@ -0,0 +1,39 @@
+//
+//  Application.h
+//  MetalSamples
+//
+//  Created by Yongsik Im on 2/2/25.
+//
+#ifndef __HS_APPLICATION_H__
+#define __HS_APPLICATION_H__
+
+#include "Precompile.h"
+
+#include "Engine/ThirdParty/ImGui/imgui.h"
+#include "SDL3/SDL.h"
+
+HS_NS_BEGIN
+
+class Renderer;
+class Scene;
+
+
+class Application
+{
+public:
+    void Init();
+    void Run();
+    void Shutdown();
+    
+private:
+    void update(float dt);
+    void render();
+    void renderGUI();
+    
+    Renderer* _renderer;
+    Scene* _scene;
+};
+
+HS_NS_END
+
+#endif
