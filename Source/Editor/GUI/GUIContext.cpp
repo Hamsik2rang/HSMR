d6d214544294fbc8653e9a994528f60c03223ce0
Yongsik Im <lvhi7121@gmail.com>
2025-02-10 00:59:51 +0900

    ImGui Style 설정

--- Source/Editor/GUI/GUIContext.cpp
+++ Source/Editor/GUI/GUIContext.cpp
@@ -45,7 +45,7 @@ void GUIContext::Initialize()
     chmod(io.IniFilename, S_IRWXU);
 
     // Setup style
-    ImGui::StyleColorsDark();
+    SetColorTheme(true);
     // ImGui::StyleColorsLight();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
@@ -71,13 +71,90 @@ void GUIContext::Finalize()
 
 void GUIContext::SetColorTheme(bool useWhite)
 {
+    auto& styles = ImGui::GetStyle();
+    
+    styles.WindowRounding = 7.0f;
+    
+    auto& colors = styles.Colors;
     if (useWhite)
     {
         ImGui::StyleColorsLight();
+        
+        colors[ImGuiCol_WindowBg] =  ImVec4(0.80f, 0.80f, 0.80f, 1.0f);
+        colors[ImGuiCol_PopupBg] = ImVec4(0.80f, 0.80f,0.80f, 1.0f);
+        
+        colors[ImGuiCol_Header]        =  ImVec4(0.76f, 0.76f, 0.76f, 1.0f);
+        colors[ImGuiCol_HeaderHovered] = ImVec4{0.63, 0.63f, 0.63f, 1.0f};
+        colors[ImGuiCol_HeaderActive]  = ImVec4(0.59f, 0.59f, 0.59f, 1.0f);
+
+        // Buttons
+        colors[ImGuiCol_Button]        =  ImVec4(0.72f, 0.72f, 0.72f, 1.0f);
+        colors[ImGuiCol_ButtonHovered] = ImVec4{0.63, 0.63f, 0.63f, 1.0f};
+        colors[ImGuiCol_ButtonActive]  = ImVec4(0.59f, 0.59f, 0.59f, 1.0f);
+
+        // Frame BG
+        colors[ImGuiCol_FrameBg]        = ImVec4{0.85f, 0.85f, 0.85f, 1.0f};
+        colors[ImGuiCol_FrameBgHovered] = ImVec4{0.9f, 0.9f, 0.9f, 1.0f};
+        colors[ImGuiCol_FrameBgActive]  = ImVec4{0.75f, 0.75f, 0.75f, 1.0f};
+        
+        // Tabs
+        colors[ImGuiCol_Tab]                = ImVec4{0.588f, 0.588f, 0.588f, 1.0f};
+        colors[ImGuiCol_TabHovered]         = ImVec4{0.75f, 0.745f, 0.75f, 1.0f};
+        colors[ImGuiCol_TabActive]          = ImVec4{0.9f, 0.9f, 0.9f, 1.0f};
+        colors[ImGuiCol_TabSelectedOverline] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+        colors[ImGuiCol_TabDimmedSelected] =  ImVec4(0.785f, 0.785f, 0.785f, 1.0f);
+        colors[ImGuiCol_TabDimmedSelected] =  ImVec4(0.81f, 0.81f, 0.81f, 1.0f);
+//        colors[ImGuiCol_TabUnfocused]       = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+//        colors[ImGuiCol_TabUnfocusedActive] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+        
+        colors[ImGuiCol_TitleBg]          = ImVec4{0.88f, 0.875f, 0.88f, 1.0f};
+        colors[ImGuiCol_TitleBgActive]    = ImVec4{0.92f, 0.92f, 0.92f, 1.0f};
+        colors[ImGuiCol_TitleBgCollapsed] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+        
+        colors[ImGuiCol_TableHeaderBg] =ImVec4(0.87f, 0.87f, 0.87f, 1.0f);
+        colors[ImGuiCol_TableBorderStrong] = ImVec4(0.568f, 0.568f, 0.568f, 1.0f);
+        colors[ImGuiCol_TableBorderLight] = ImVec4(0.678f, 0.678f, 0.678f, 1.0f);
+        
+        colors[ImGuiCol_CheckMark] = ImVec4(0.15f, 0.15f, 0.15f, 1.0f);
+        colors[ImGuiCol_DockingPreview] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+        
+        colors[ImGuiCol_SliderGrab] = ImVec4(0.69f, 0.69f, 0.69f, 1.0f);
+        colors[ImGuiCol_SliderGrabActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.0f);
+        
+        colors[ImGuiCol_PlotHistogram] = ImVec4(0.26f, 0.59f, 1.00f, 0.00f);
     }
     else
     {
         ImGui::StyleColorsDark();
+        
+        colors[ImGuiCol_WindowBg] = ImVec4{0.1f, 0.105f, 0.11f, 1.0f};
+
+        // Headers
+        colors[ImGuiCol_Header]        = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+        colors[ImGuiCol_HeaderHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+        colors[ImGuiCol_HeaderActive]  = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+        // Buttons
+        colors[ImGuiCol_Button]        = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+        colors[ImGuiCol_ButtonHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+        colors[ImGuiCol_ButtonActive]  = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+        // Frame BG
+        colors[ImGuiCol_FrameBg]        = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+        colors[ImGuiCol_FrameBgHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+        colors[ImGuiCol_FrameBgActive]  = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+        // Tabs
+        colors[ImGuiCol_Tab]                = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+        colors[ImGuiCol_TabHovered]         = ImVec4{0.38f, 0.3805f, 0.381f, 1.0f};
+        colors[ImGuiCol_TabActive]          = ImVec4{0.28f, 0.2805f, 0.281f, 1.0f};
+        colors[ImGuiCol_TabUnfocused]       = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+        colors[ImGuiCol_TabUnfocusedActive] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+
+        // Title
+        colors[ImGuiCol_TitleBg]          = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+        colors[ImGuiCol_TitleBgActive]    = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+        colors[ImGuiCol_TitleBgCollapsed] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
     }
 }
 
