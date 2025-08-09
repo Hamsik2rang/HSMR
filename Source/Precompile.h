931ca2de2019f470a35f62afaf297ff3d1e00bb8
Yongsik Im <lvhi7121@gmail.com>
2025-08-09 10:49:44 +0900

    feat: modify architecture layer

--- Source/Precompile.h
+++ Source/Precompile.h
@@ -75,32 +75,104 @@ typedef uint64_t uint64;
 #if defined(HS_API_EXPORT)
     #if defined(__APPLE__)
         #define HS_API __attribute__((__visibility__("default")))
+
+        #if defined(HS_HAL_API_EXPORT)
+            #define HS_HAL_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_CORE_API_EXPORT)
+            #define HS_CORE_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_RESOURCE_API_EXPORT)
+            #define HS_RESOURCE_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_SHADERSYSTEM_API_EXPORT)
+            #define HS_SHADERSYSTEM_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_RHI_API_EXPORT)
+            #define HS_RHI_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_RENDERER_API_EXPORT)
+            #define HS_RENDERER_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_ECS_API_EXPORT)
+            #define HS_ECS_API __attribute__((__visibility__("default")))
+        #endif
+
+        #if defined(HS_EDITOR_API_EXPORT)
+            #define HS_ECS_API __attribute__((__visibility__("default")))
+        #endif
     #else
         #define HS_API __declspec(dllexport)
+
+        #if defined(HS_HAL_API_EXPORT)
+            #define HS_HAL_API _declspec(dllexport)
+        #endif
+
+        #if defined(HS_CORE_API_EXPORT)
+            #define HS_CORE_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_RESOURCE_API_EXPORT)
+            #define HS_RESOURCE_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_SHADERSYSTEM_API_EXPORT)
+            #define HS_SHADERSYSTEM_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_RHI_API_EXPORT)
+            #define HS_RHI_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_RENDERER_API_EXPORT)
+            #define HS_RENDERER_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_ECS_API_EXPORT)
+            #define HS_ECS_API __declspec(dllexport)
+        #endif
+
+        #if defined(HS_EDITOR_API_EXPORT)
+            #define HS_ECS_API __declspec(dllexport)
+        #endif
     #endif
 #elif defined(HS_API_IMPORT)
     #if defined(__APPLE__)
         #define HS_API
-    #else
-        #define HS_API __declspec(dllimport)
-    #endif
-#else
-    #define HS_API
-#endif
-
-#if defined(HS_EDITOR_API_EXPORT)
-    #if defined(__APPLE__)
-        #define HS_EDITOR_API __attribute__((__visibility__("default")))
-    #else
-        #define HS_EDITOR_API __declspec(dllexport)
-    #endif
-#elif defined(HS_EDITOR_API_IMPORT)
-    #if defined(__APPLE__)
+        #define HS_HAL_API
+        #define HS_CORE_API
+        #define HS_RESOURCE_API
+        #define HS_SHADERSYSTEM_API
+        #define HS_RHI_API
+        #define HS_RENDERER_API
+        #define HS_ECS_API
         #define HS_EDITOR_API
     #else
+        #define HS_API __declspec(dllimport)
+        #define HS_HAL_API __declspec(dllimport)
+        #define HS_CORE_API __declspec(dllimport)
+        #define HS_RESOURCE_API __declspec(dllimport)
+        #define HS_SHADERSYSTEM_API __declspec(dllimport)
+        #define HS_RHI_API __declspec(dllimport)
+        #define HS_RENDERER_API __declspec(dllimport)
+        #define HS_ECS_API __declspec(dllimport)
         #define HS_EDITOR_API __declspec(dllimport)
     #endif
 #else
+    #define HS_API
+    #define HS_HAL_API
+    #define HS_CORE_API
+    #define HS_RESOURCE_API
+    #define HS_SHADERSYSTEM_API
+    #define HS_RHI_API
+    #define HS_RENDERER_API
+    #define HS_ECS_API
     #define HS_EDITOR_API
 #endif
 
